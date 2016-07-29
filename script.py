119c0e93898c4660b40da0063714198901ee6e40
unknown <jmoy@PK14YA1.wcs.org>
2016-07-29 17:36:26 -0400

    move ELEMENTS to mw_settings

--- script.py
+++ script.py
@@ -14,7 +14,7 @@ COMBINATION = 1
 SUBSET = 2
 ADJACENCY = 6
 CONDITIONAL = 0
-elements = {}
+
 num_to_map = {1: 0,
               2: 0,
               3: 0,
@@ -24,46 +24,51 @@ num_mapped = 0
 
 
 # convert json objects to mw.element objects and key by element id
-for i in db['elements']:
+for i in mw_db['elements']:
     # print i
     e = mw.json_element_to_object(i)
-    elements[e.id] = e
+    s.ELEMENTS[e.id] = e
 
 # store relationship records in their respective subject elements
-for i in db['relationships']:
+for i in mw_db['relationships']:
     # print i
     r = mw.json_relationship_to_object(i)
-    elements[r.subject].relationships.append(r)
-    elements[r.subject].object_list.append(r.object)
+    s.ELEMENTS[r.subject].relationships.append(r)
+    if r.object not in s.ELEMENTS[r.subject].object_list:
+        s.ELEMENTS[r.subject].object_list.append(r.object)
+s.ELEMENTS[112.2].check_status()
+s.ELEMENTS[112.2].show_attributes()
+print s.ELEMENTS[112.2].path
 
-# elements[116.1].show_attributes()
-
-for id in elements:
-    elements[id].set_grid()
-    if elements[id].grid is None and elements[id].automap == True:
-        if elements[id].definition in num_to_map:
-            num_to_map[elements[id].definition] += 1
-        else:
-            num_to_map[elements[id].definition] = 1
-    else:
-        num_mapped += 1
-
-pp(num_to_map)
-print num_mapped
-
-
-elements[54.10].sort_relationships(elements)
-elements[54.10].show_requirements()
-# map distribution
+s.ELEMENTS[644.0].show_attributes()
+s.ELEMENTS[644.0].check_requirements()
 # for id in elements:
+#     elements[id].set_grid()
+#     if elements[id].grid is None and elements[id].automap == True:
+#         if elements[id].definition in num_to_map:
+#             num_to_map[elements[id].definition] += 1
+#         else:
+#             num_to_map[elements[id].definition] = 1
+#     else:
+#         num_mapped += 1
+
+# pp(num_to_map)
+# print num_mapped
+
+
+# s.ELEMENTS[54.10].sort_relationships()
+# s.ELEMENTS[54.10].show_requirements()
+# map distribution
+
+# for id in s.ELEMENTS:
 #
-#     if elements[id].status == 0:
+#     if s.ELEMENTS[id].status == 0:
 #
-#         if elements[id].definition == COMBINATION:
-#             mw.combination(elements[id])
+#         if s.ELEMENTS[id].definition == COMBINATION:
+#             mw.combination(s.ELEMENTS[id])
 #
-#         elif elements[id].definition == SUBSET:
-#             mw.subset(elements[id])
+#         elif s.ELEMENTS[id].definition == SUBSET:
+#             mw.subset(s.ELEMENTS[id])
 #
-#         elif elements[id].definition == ADJACENCY:
-#             mw.adjacency(elements[id])
+#         elif s.ELEMENTS[id].definition == ADJACENCY:
+#             mw.adjacency(s.ELEMENTS[id])
