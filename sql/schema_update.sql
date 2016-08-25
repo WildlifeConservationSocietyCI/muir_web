9560e34801603209bb0b3884b8bfd0b384116077
Jesse Moy <jmoy@wcs.org>
2016-08-25 16:39:46 -0400

    added comments and relationship_type refactoring statement

--- sql/schema_update.sql
+++ sql/schema_update.sql
@@ -36,9 +36,11 @@ UPDATE welikia_mw_definitiontype
    SET description = 'subset'
  WHERE id = 2;
 
+
 UPDATE welikia_mw_definitiontype
    SET description = 'adjacency'
  WHERE id = 3;
+
 
 DELETE
   FROM welikia_mw_definitiontype
@@ -67,29 +69,75 @@ UPDATE welikia_mw_element AS e
            AND (d.description IS NOT NULL)
            AND (e.automap = TRUE));
 
+
 ALTER TABLE welikia_mw_element ADD COLUMN adjacency_rule integer;
 
 UPDATE welikia_mw_element AS e
    SET adjacency_rule =
-       (SELECT d.description::integer
-	  FROM welikia_mw_element_description AS d
-	 WHERE (d.id = e.description_id
-  	   AND e.mw_definition = 3)
-	   AND (d.description IS NOT NULL)
-	   AND (e.automap = TRUE)
-	   AND (d.description ~ E'^\\d+$'));
+	   (SELECT d.description::integer
+		  FROM welikia_mw_element_description AS d
+		 WHERE (d.id = e.description_id
+  	       AND e.mw_definition = 3)
+		   AND (d.description IS NOT NULL)
+		   AND (e.automap = TRUE)
+		   AND (d.description ~ E'^\\d+$'));
 
- -- ADD POLARITY TO RELATIONSHIP
+-- IMPLEMENT RELATIONSHIP GROUP
+/*
+this statement will be written by Kim
+*/
 
-ALTER TABLE welikia_mw_relationship ADD COLUMN polarity boolean NOT NULL DEFAULT TRUE;
+-- IMPLEMENT RELATIONSHIP TYPE
+/*
+"relationship_type" was previously used to group similar kinds of
+requirements (food, water, shelter).
 
+This field now refers to the direction of an object's effect on its
+subject. Enhancing and attenuating types scale the "core habitat"
+(required) in positive and negative directions respectivly. The type
+will point relationships to the appropriate arethmetic operation.
+
+The field will filled using old strength_types
+
+relationship_types:
+	required = 0		(object * weigth)
+	enhancing = 1		1 + (object * weight)
+	attenuating = 2 	1 - (object * weight)
+*/
+
+ALTER TABLE welikia_mw_relationship ADD COLUMN relationship_type integer NOT NULL;
+
+-- update required relationship_type
 UPDATE welikia_mw_relationship
-   SET polarity = FALSE
- WHERE strengthtype = 2
-    OR strengthtype = 4;
+   SET relationship_type = 0
+ WHERE strengthtype = 0  -- central
+    OR strengthtype = 3  -- required
+    OR strengthtype = 5  -- central
+    OR strengthtype = 6; -- subcentral
+
+-- update enhancing relationship_type
+UPDATE welikia_mw_relationship
+   SET relationship_type = 1
+ WHERE strengthtype = 1;  -- attenuating
+
+-- update attenuating relationship_type
+UPDATE welikia_mw_relationship
+   SET relationship_type = 2
+ WHERE strengthtype = 2  -- attenuating
+    OR strengthtype = 4; -- exclusionary
 
 -- REFACTOR STRENGTH TYPES
--- new strength scale: 0 = 0.25, 1 = 0.50, 2 = 0.75, 3 = 1.0
+/*
+new schema seperates the idea of strength and type.
+ie. a relationship can be strongly attenuating or strongly enhancing.
+the new strength is a scale ranging from weak -> strong.
+only refactor after the new relationship_type field is populated
+new strength scale:
+	0 = 0.25,
+	1 = 0.50
+	2 = 0.75
+	3 = 1.00
+*/
 
 UPDATE welikia_mw_relationship
    SET strengthtype_id = 2
@@ -98,9 +146,11 @@ UPDATE welikia_mw_relationship
 
 
 UPDATE welikia_mw_relationship
-   SET strengthtype_id = 2
- WHERE strengthtype_id = 6  -- subcentral
-    OR strengthtype_id = 2; -- attenuating
+   SET strengthtype_id = 3
+ WHERE strengthtype_id = 0  -- central
+    OR strengthtype_id = 3  -- required
+    OR strengthtype_id = 4  -- exclusionary
+    OR strengthtype_id = 5; -- central
 
 
 -- DROP UNUSED RELATIONSHIP ATTIBUTES
