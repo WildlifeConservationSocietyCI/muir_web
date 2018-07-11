286cb09f38699941e1b24f2f394f5679723cdc5c
Kim Fisher <kfisher@wcs.org>
2018-07-11 11:48:21 -0400

    remove nodata coercion in raster to array conversion

--- raster_utils.py
+++ raster_utils.py
@@ -21,22 +21,16 @@ def raster_to_ndarray(in_raster):
     src_ds = gdal.Open(in_raster, GA_ReadOnly)
     geotransform, projection, nodata = get_geo_info(src_ds)
     array = gdal_array.DatasetReadAsArray(src_ds)
-    # print(array.dtype)
-    # print(np.issubdtype(array.dtype, np.integer))
-    print(nodata)
-    print(array)
     array = np.ma.masked_values(array, nodata)
-    # array = np.ma.masked_equal(array, nodata)
-    print(array)
     # array = np.ma.masked_equal(array, nodata)  # int only
     # Force nodata value to avoid divergent input nodata values
-    datatype = src_ds.GetRasterBand(1).DataType
-    if datatype == gdal.GDT_Int16:
-        nodata = s.NODATA_INT16
-    elif datatype == gdal.GDT_Float32:
-        nodata = s.NODATA_FLOAT32
-    else:
-        logging.exception('Raster data type %s not implemented' % datatype)
+    # datatype = src_ds.GetRasterBand(1).DataType
+    # if datatype == gdal.GDT_Int16:
+    #     nodata = s.NODATA_INT16
+    # elif datatype == gdal.GDT_Float32:
+    #     nodata = s.NODATA_FLOAT32
+    # else:
+    #     logging.exception('Raster data type %s not implemented' % datatype)
     array.set_fill_value(nodata)
 
     src_ds = None
@@ -46,8 +40,6 @@ def raster_to_ndarray(in_raster):
 def ndarray_to_raster(array, out_raster):
     y_size, x_size = array.shape
     dtype = gdal_array.NumericTypeCodeToGDALTypeCode(array.dtype)
-    # print(array.dtype)
-    # print(dtype)
     output_raster = gdal.GetDriverByName(s.RASTER_DRIVER).Create(out_raster['file'], x_size, y_size, 1, dtype)
 
     output_raster.SetGeoTransform(out_raster['geotransform'])
@@ -55,18 +47,9 @@ def ndarray_to_raster(array, out_raster):
     srs.ImportFromWkt(out_raster['projection'])
     output_raster.SetProjection(srs.ExportToWkt())
 
-    # nodata = float(out_raster['nodata'])
-    # array.set_fill_value(nodata)
     array.set_fill_value(out_raster['nodata'])
-    logging.info(array)
-    logging.info(array.dtype)
-    logging.info(array.mask)
-    logging.info(array.fill_value)
-    logging.info(out_raster['nodata'])
-    # print(nodata)
     output_raster.GetRasterBand(1).WriteArray(array)
     output_raster.GetRasterBand(1).SetNoDataValue(out_raster['nodata'])
-    # SetNoDataValue(np.float64(nodata))
 
     out_raster = None
     ourput_raster = None
